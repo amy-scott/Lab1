e2b75ff0aa1c4445c053d477a8b7736e7bfb77c9
Dell User <Dell User@DESKTOP-AHC4TQ0>
2020-02-11 22:57:20 -0600

    finished project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Dell User
- */
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter the amount of your purchase:");
+    double Amount = keyboard.nextDouble();
+    double StateTaxRate = 0.04;
+    double CountyTaxRate = 0.02;
+    System.out.println("The amount is " + Amount);
+    double StateTax = (StateTaxRate * Amount);
+    double CountyTax = (CountyTaxRate * Amount);
+    double SalesTaxTotal = (StateTax + CountyTax);
+    double TotalSale = (Amount + SalesTaxTotal);
+    double StateTaxRoundoff = Math.round(StateTax * 100.0) / 100.0;
+    double CountyTaxRoundoff = Math.round(CountyTax * 100.0) / 100.0;
+    double SalesTaxTotalRoundoff = Math.round(SalesTaxTotal * 100.0) / 100.0;
+    double TotalSaleRoundoff = Math.round(TotalSale * 100.0) / 100.0;
+    System.out.println("The state tax is: " + StateTaxRoundoff);
+    System.out.println("The county tax is: " + CountyTaxRoundoff);
+    System.out.println("The total sales tax is: " + SalesTaxTotalRoundoff);
+    System.out.println("The total of this sale is: " + TotalSaleRoundoff);
     
+    
+   
+    
+    
+            
+    }
 }
