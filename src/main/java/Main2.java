b3ca039355b1e4dc1933e01dd4d25164a6cf10f3
Dell User <Dell User@DESKTOP-AHC4TQ0>
2020-02-11 23:17:13 -0600

    finished

---
+++ src/main/java/Main2.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+public class Main2 {
+   
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Please enter the number of cookies consumed: ");
+        int CookiesEaten = keyboard.nextInt();
+        int Total = 40;
+        int Servings = 10;
+        float CookiesPerServing = (Total/Servings);
+        int CaloriesPerServing = 300;
+        float ServingsEaten = (CookiesEaten/CookiesPerServing);
+        float CaloriesEaten = (ServingsEaten*CaloriesPerServing);
+        System.out.println("You consumed " + CaloriesEaten + " calories.");
+        
+    }
+}
